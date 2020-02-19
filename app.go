7d1d88165947ed5fa7c92c47760d63e82d1a714f
Wechuli <wechulipaul@yahoo.com>
2020-02-19 10:45:12 +0300

    handler to get all products

--- app.go
+++ app.go
@@ -69,9 +69,20 @@ func (a *App) getProducts(w http.ResponseWriter, r *http.Request) {
 	count, _ := strconv.Atoi(r.FormValue("count"))
 	start, _ := strconv.Atoi(r.FormValue("start"))
 
-	if count > 10 || count < 1{
+	if count > 10 || count < 1 {
 		count = 10
 	}
 
-	if start < 0
+	if start < 0 {
+		start = 0
+	}
+
+	products, err := getProducts(a.DB, start, count)
+
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, products)
 }
