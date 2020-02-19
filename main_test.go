06997adb74beb1207617abbbbeefee20dacc6f8a
Wechuli <wechulipaul@yahoo.com>
2020-02-19 09:55:58 +0300

    adding test to delete product

--- main_test.go
+++ main_test.go
@@ -176,3 +176,21 @@ func TestUpdateProduct(t *testing.T) {
 	}
 
 }
+
+func TestDeleteProduct(t *testing.T) {
+	clearTable()
+	addProducts(1)
+
+	req, _ := http.NewRequest("GET", "/product/1", nil)
+	response := executeRequest(req)
+	checkResponseCode(t, http.StatusOK, response.Code)
+
+	req, _ = http.NewRequest("DELETE", "/product/1", nil)
+	response = executeRequest(req)
+
+	checkResponseCode(t, http.StatusOK, response.Code)
+
+	req, _ = http.NewRequest("GET", "/product/1", nil)
+	response = executeRequest(req)
+	checkResponseCode(t, http.StatusNotFound, response.Code)
+}
