37107ae75e77e449a64fb12207e29e0be3149dd6
Wechuli <wechulipaul@yahoo.com>
2020-02-19 10:42:35 +0300

    route handlers for the products

--- app.go
+++ app.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
+	"strconv"
+
 	"github.com/gorilla/mux"
 	_ "github.com/lib/pq"
 )
@@ -15,7 +19,6 @@ type App struct {
 
 func (a *App) Initialize(host string, port int, user string, password string, dbname string) {
 
-
 	var err error
 	connectionString := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
 	a.DB, err = sql.Open("postgres", connectionString)
@@ -26,3 +29,49 @@ func (a *App) Initialize(host string, port int, user string, password string, db
 
 }
 func (a *App) Run(addr string) {}
+
+func (a *App) getProduct(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid product ID")
+		return
+	}
+
+	p := product{ID: id}
+
+	if err := p.getProduct(a.DB); err != nil {
+		switch err {
+		case sql.ErrNoRows:
+			respondWithError(w, http.StatusNotFound, "Product not found")
+		default:
+			respondWithError(w, http.StatusInternalServerError, err.Error())
+		}
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, p)
+}
+
+func respondWithError(w http.ResponseWriter, code int, message string) {
+	respondWithJSON(w, code, map[string]string{"error": message})
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	response, _ := json.Marshal(payload)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(response)
+}
+
+func (a *App) getProducts(w http.ResponseWriter, r *http.Request) {
+	count, _ := strconv.Atoi(r.FormValue("count"))
+	start, _ := strconv.Atoi(r.FormValue("start"))
+
+	if count > 10 || count < 1{
+		count = 10
+	}
+
+	if start < 0
+}
